dbe20e52966441a486b7255789b61c9624f9bd76
Nico <nbl0357@gmail.com>
2021-07-10 12:14:38 -0400

    Updated Program.cs
    Calculates and Outputs the SHA1 hash of the input String

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Pwned_Password_Search
 {
@@ -7,6 +9,36 @@ namespace Pwned_Password_Search
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter your password: ");
+
+            String input = Console.ReadLine();
+
+            if (input == null)
+            {
+                return;
+            }
+
+            //Convert the String into a byte array
+            Byte[] DataArray;
+
+            DataArray = Encoding.ASCII.GetBytes(input);
+
+            //Compute the hash of DataArray
+            HashAlgorithm SHA = SHA1.Create();
+
+            byte[] result = SHA.ComputeHash(DataArray);
+
+            //Loop through each byte and convert it into a hexadecimal String
+            StringBuilder StrBuild = new StringBuilder(result.Length * 2);
+
+            foreach (byte ele in result)
+            {
+                StrBuild.AppendFormat("{0:x2}", ele);
+            }
+
+            String HexString = StrBuild.ToString();
+
+            Console.Write(HexString);
+
         }
     }
 }
